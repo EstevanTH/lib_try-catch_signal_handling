689e361162c7f09ce40447adff8b349282466df1
Mohamed RACHID <EstevanTH@users.noreply.github.com>
2018-06-03 15:41:58 +0200

    Improved main.cpp example
    - New genuine SIGILL signal using the HLT instruction (for GCC and MSVC)
    - New messages for more explicit operations
    - Additional empty lines for better view
    - Removed the deleted by optimization integer division by 0

--- main.cpp
+++ main.cpp
@@ -14,63 +14,83 @@ int main(/*int argc, char *argv[]*/){
 	std::cout << "SIGSEGV = " << SIGSEGV << std::endl;
 	std::cout << "SIGILL = " << SIGILL << std::endl;
 	std::cout << "SIGFPE = " << SIGFPE << std::endl;
+	std::cout << std::endl;
 	
 	// Let's do several illegal operations and let's see!
 	TRY{
+		std::cout << "Attempting to dereference a null pointer..." << std::endl;
 		TRY{
 			// TryCatchSignalHandler::signalHandler() gets called if application not wrapped.
 			int* ptr = 0;
 			std::cout << "*ptr = " << *ptr << std::endl; // invalid memory access #1
+			std::cout << "Unexpected success!" << std::endl;
 		}END_TRY()
 		catch( std::exception& e ){
 			MESSAGE_EXCEPTION( e.what(), __FILE__, __LINE__ );
 		}
 		FINISH_TRY();
+		std::cout << std::endl;
 		
+		std::cout << "Attempting to dereference a null pointer again..." << std::endl;
 		TRY{
 			// TryCatchSignalHandler::signalHandler() gets called if application not wrapped.
 			int* ptr = 0;
 			std::cout << "*ptr = " << *ptr << std::endl; // invalid memory access #2
+			std::cout << "Unexpected success!" << std::endl;
 		}END_TRY()
 		catch( std::exception& e ){
 			MESSAGE_EXCEPTION( e.what(), __FILE__, __LINE__ );
 		}
 		FINISH_TRY();
+		std::cout << std::endl;
 		
+		#if defined( __GNUC__ ) || defined( _MSC_VER )
+			std::cout << "Attempting forbidden HLT instruction, press a key if frozen..." << std::endl;
+			TRY{
+				// TryCatchSignalHandler::signalHandler() gets called if application not wrapped.
+				#ifdef __GNUC__
+					__asm__( "HLT" ); // forbidden CPU instruction
+				#endif
+				#ifdef _MSC_VER
+					__asm HLT; // forbidden CPU instruction
+				#endif
+				std::cout << "The HLT instruction actually worked!" << std::endl;
+			}END_TRY()
+			catch( std::exception& e ){
+				MESSAGE_EXCEPTION( e.what(), __FILE__, __LINE__ );
+			}
+			FINISH_TRY();
+			std::cout << std::endl;
+		#endif
+		
+		std::cout << "Simulating a SIGFPE signal..." << std::endl;
 		TRY{
 			// TryCatchSignalHandler::signalHandler() gets called, program continues without needing a jump (because simulated).
-			std::raise( SIGILL ); // invalid instruction (simulated)
+			std::raise( SIGFPE ); // invalid instruction (simulated)
+			std::cout << "Unexpected success!" << std::endl;
 		}END_TRY()
 		catch( std::exception& e ){
 			MESSAGE_EXCEPTION( e.what(), __FILE__, __LINE__ );
 		}
 		FINISH_TRY();
+		std::cout << std::endl;
 		
-		TRY{
-			// TryCatchSignalHandler::signalHandler() gets called if application not wrapped.
-			// This code is often ignored because of optimization.
-			int a = 1;
-			int b = 0;
-			a = a/b; // invalid math operation
-		}END_TRY()
-		catch( std::exception& e ){
-			MESSAGE_EXCEPTION( e.what(), __FILE__, __LINE__ );
-		}
-		FINISH_TRY();
-		
+		std::cout << "Attempting an integer division by 0..." << std::endl;
 		do{
 			// TryCatchSignalHandler::signalHandler() gets called if application not wrapped.
 			int a = 1;
 			int b = 0;
 			std::cout << "res = " << a/b << std::endl; // invalid math operation
+			std::cout << "Unexpected success!" << std::endl;
 		} while( false );
 		
 		std::cout << "This message is never shown." << std::endl;
 	}END_TRY()
 	catch( std::exception& e ){
-		std::cerr << "Program survived a " << e.what() << " in " << __FILE__ << ":" << __LINE__ << std::endl;
+		MESSAGE_EXCEPTION( e.what(), __FILE__, __LINE__ );
 	}
 	FINISH_TRY();
+	std::cout << std::endl;
 	
 	// Prove that we are still alive after all these CPU exceptions:
 	std::cout << "Enter a character now: ";
